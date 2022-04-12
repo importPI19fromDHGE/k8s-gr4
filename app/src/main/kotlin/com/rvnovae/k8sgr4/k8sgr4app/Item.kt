fe4e88a8698da5f7e41da77986ef1d678e717e7e
Oliver Kogel <kogel.oliver@web.de>
2022-04-12 08:47:58 +0200

    fix table name and ddl

--- app/src/main/kotlin/com/rvnovae/k8sgr4/k8sgr4app/Item.kt
+++ app/src/main/kotlin/com/rvnovae/k8sgr4/k8sgr4app/Item.kt
@@ -2,7 +2,7 @@ package com.rvnovae.k8sgr4.k8sgr4app
 
 import javax.persistence.*
 
-@Entity
+@Entity (name = "main")
 class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
