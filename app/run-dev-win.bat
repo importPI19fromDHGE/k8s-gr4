52e41725adcb0e73292ae35c223fb5726dd999c5
photovoltex <36958734+photovoltex@users.noreply.github.com>
2022-04-13 16:43:39 +0200

    cleanup and build from scratch to reduce image even more 🔥

--- app/run-dev-win.bat
+++ app/run-dev-win.bat
@@ -1,2 +1,1 @@
-# exec container from img
-docker run --detach -p 80:8080 --env K8SGR4_LOG=trace --env MARIADB_HOST=host.docker.internal:3306 --env MARIADB_DATABASE=todolist --env MARIADB_USER=some-user --env MARIADB_PASSWORD=some-pwd --env MARIADB_TABLE=main k8s-gr4/app-rust
+docker run --detach --name k8s-gr4-dev-win -p 8080:8080 --env K8SGR4_LOG=debug --env MARIADB_HOST=host.docker.internal:3306 --env MARIADB_DATABASE=todolist --env MARIADB_USER=some-user --env MARIADB_PASSWORD=some-pwd --env MARIADB_TABLE=main k8s-gr4/app-rust
