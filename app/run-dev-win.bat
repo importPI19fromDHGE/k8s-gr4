6c72d795bd8e2322cf271bafb6934fa664b41101
Oliver Kogel <kogel.oliver@web.de>
2022-04-14 08:30:35 +0200

    format windows run dev script

--- app/run-dev-win.bat
+++ app/run-dev-win.bat
@@ -1,1 +1,10 @@
-docker run --detach --name k8s-gr4-dev-win -p 8080:8080 --env K8SGR4_LOG=debug --env MARIADB_HOST=host.docker.internal:3306 --env MARIADB_DATABASE=todolist --env MARIADB_USER=some-user --env MARIADB_PASSWORD=some-pwd --env MARIADB_TABLE=main k8s-gr4/app-rust
+docker run --detach ^
+    --name k8s-gr4-dev-win ^
+    -p 8080:8080 ^
+    --env K8SGR4_LOG=debug ^
+    --env MARIADB_HOST=host.docker.internal:3306 ^
+    --env MARIADB_DATABASE=todolist ^
+    --env MARIADB_USER=some-user ^
+    --env MARIADB_PASSWORD=some-pwd ^
+    --env MARIADB_TABLE=main ^
+    k8s-gr4/app-rust
