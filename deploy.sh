423d7613a74f9c1027532d906c9a3b01ec7aa143
Felix Prillwitz <36958734+photovoltex@users.noreply.github.com>
2022-05-05 16:12:12 +0200

    deploy should work

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-addresses='$(cat deploy-nodes)'
 kubectl apply -f https://raw.githubusercontent.com/haproxytech/kubernetes-ingress/v1.7/deploy/haproxy-ingress.yaml
-kubectl patch deployment haproxy-kubernetes-ingress -n haproxy-controller -p '{ "spec": { "template": { "containers": { "args": [ "--ingress.class=haproxy" ] } } } }'
-kubectl patch service haproxy-kubernetes-ingress -n haproxy-controller --patch-file deploy-nodes.yaml
+kubectl patch deployment haproxy-kubernetes-ingress -n haproxy-controller --patch-file patch-ingress-controller.yaml
+kubectl patch service haproxy-kubernetes-ingress -n haproxy-controller --patch-file patch-deploy-nodes.yaml
 kubectl apply -f deployment.yaml
